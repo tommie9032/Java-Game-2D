33af2d98a1969adb0132b9e02725a1d4b31ee571
tommie9032 <tomthomas9032@gmail.com>
2025-04-05 11:07:42 +0530

    Created Game Frame

---
+++ src/main/main.java
@@ -0,0 +1,17 @@
+package main;
+import javax.swing.JFrame;
+
+
+public class main {
+
+    public static void main(Stirng[] args){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(False);
+        window.setTitle("Sea of Bones");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(True);
+
+    }
+}
